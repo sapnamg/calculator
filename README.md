07128a87403b34c6d475dd0080335e3fe2d8470e
sapnamg <154641437+sapnamg@users.noreply.github.com>
2025-09-27 23:14:25 +0000

    Refactored calculator to use class-based structure and updated README

--- README.md
+++ README.md
@@ -1,34 +1,43 @@
-# 🔢 #100Devs Push Project001: Calculator
+# 🔢 100Devs Push Project001: Calculator
 
 ## Description
 A simple calculator built with HTML, CSS, and JS.
 
-Features
+## Features
 - Clickable buttons for numbers and operators
 - Prevents multiple decimals in a number
 - Allows negative numbers (must enter `-` as the first input)
 - Prevents inputting multiple operators in a row (last entered operator overwrites others)
 - Evaluates expressions with correct order of operations
-- Dynamic font resizing to fit results on the display
+- Automatically resizes font to fit results on the display
 - Clear button to reset everything
+- Refactored to use classes for better organization and use of OOP principles
 
-How It Works
-- Numbers and operators are stored in an array as you click.
-- Pressing `=` evaluates the array
+## How It Works
+- Calculator class creates a calculator that stores numbers and operators in an array as you click on the web interface 
+- Evaluator class is instantiated when `"="` is clicked to evaluate the expression array
+- Evaluator loops through array searching for operators and executing them according to mathematical order of operations
 - The result is displayed on screen, font size adjusts automatically if needed to fit in the results window
 
-Project Structure
-- `index.html`: main HTML file
-- `style.css`: calculator styling
-- `script.js`: calculator functions and interaction handling
+![Calculator Screenshot](screenshot.jog)
 
-Usage
+## Project Structure
+- `index.html`: main HTML file
+- `myStyleSheet.css`: calculator styling
+- `main.js`: calculator functions and interaction handling
+
+## Usage
 1. Clone or download this repository
 2. Open `index.html` in your browser
 3. Start clicking buttons to calculate something!
 
-Limitations and areas for future improvements:
-- Handling of undefined results only gives "Infinity" as an answer currently
+## Limitations and areas for future improvements:
 - Keyboard input is not supported
 - No parentheses or advanced operations available
-- Object-based operator methods will be added
+
+## Changelog
+- v1.0
+    - Initial calculator with functional structure
+- v1.1
+    - Converted calculator from functional style to class-based structure
+    - Fixed display of "Infinity" for division by 0; now displays "Undefined"
--- main.js
+++ main.js
@@ -1,174 +1,181 @@
-//
-//1. user clicks numbers and operators
-//2. save number and operator in variables
-//3. ensure that operators can't be entered twice in a row (they will overwrite the previous one)
-//3a. Except minus sign allows negating value
-//4. ensure that decimal only occurs once in a number (ie only once before operators is input)
-//5. once equal button is clicked, evaluate expression and return answer
-//6. evaluation must follow pemdas
-//7. display answer in screen
-
-//8. refactor make font size dynamic on screen 
-//9. refactor - can this be done with an ojbect of operator methods?
-//----------------------------------------Saving values on clicks and text input-----------------------------------------------------------------------------//
-let value=""; //will store value of each operand when selected
-let valuesArray=[]; //will store each part of expression in an array
-let operator; //will store each operator when selected
-let result; //will store final answer
-let numberSelector=document.querySelectorAll(".number");//refer to all number buttons and "."
-let operatorSelector=document.querySelectorAll(".operator");//refer to all operator buttons
-let screen=document.querySelector(".screen");
-numberSelector.forEach(item=>item.addEventListener("click", saveNum));//save number to value on click
-operatorSelector.forEach(item=>item.addEventListener("click", saveOperator));//save operator on click
-
-
-//saveNum will collect entries from calclator and store in variable value as string
-function saveNum() {
-    //make sure there is no more than 1 decimal in a number
-    if (this.value=="." && value.includes(".")) {
-        return value;
-    }
-    //concatenate additional entries onto value (value will not be overwritten, so has to be cleared later after operator selected)
-    else {
-        value+=this.value;
-    }
-    //display value on screen (will display on each click of numbers as saveNum is run)
-    screen.innerText=value;
-    adjustFontSize(screen);
-}
-
-//saveOperator will add previously stored value to array, store chosen operator, and evaluate when = is chosen
-function saveOperator(){
-    //save button's value to operator (will overwrite any previous operator)
-    operator=this.value;
-    //if "=" used, end expression and evaluate:
-    if (this.value=="=") {
-        //when = selected, add last stored value to valuesArray, if not a number, then store 0
-        if(value=="" || isNaN(+value)) {
-            console.log("no number entered, using 0");
-            value=0;
-        }
-        valuesArray.push(value);
-        //display entire array (ie the expression) on screen as a string
-        screen.innerText=valuesArray.join(' ');
-        //evaluate the expression
-        evaluate(valuesArray);
-        //clear the values array once expression is evaluated (will not allow additional calcuations on expression)
-        valuesArray=[];
-    }
-    //Allow making negative number if using "-" before entering a number. 
-        //Therefore only allow if there is no number stored in value already (doesn't matter if there is an operator stored or not)
-    else if (this.value=="-" && value=="") {
-        value=operator;
-        //console.log(value);//for visualization if entry of "-" logs as value vs operator
-        //return value to escape this function so that value is not set to ""
-        return value;
-    }
-
-    //If math operator chosen (which is not "-" at beginning of number), store to expression:
-    else {
-        //once operator is chosen, push existing value to valuesArray
-        if (value!="" && value!="-") {
-            valuesArray.push(value);
-            valuesArray.push(operator);
-            screen.innerText=operator;
-        }
-        //if there is no value entered yet in expression, prompt user to enter number
-        else if(valuesArray.length==0) {
-            screen.innerText="Enter a number";
-        }
-        //if value is either 1)empty because already used an operatore and it clear,
-        //or 2)or it is simply "-" without number, clicking operatore will mean that user wants to change operator
-        //therefore remove last element of valuesArray and push new operator
-        else if ((value=="" || value=="-") && valuesArray.length!=0){
-            console.log("changing operator");
-            valuesArray.splice(-1, 1, operator);
-            screen.innerText=operator;
-        }
+//Calculator display and input
+class Calculator {
+    
+    constructor () {
+        //set up initial calculator state
+        this.value=""; //will store value of each operand when selected
+        this.valuesArray=[]; //will store each part of expression in an array
+        this.operator; //will store each operator when selected
+        this.result; //will store final answer
         
-        //display operator on screen (similar to displaying values on screen while clicking buttons)
+        //set calculator interface and display
+        this.screen=document.querySelector(".screen");
+        this.numberSelector=document.querySelectorAll(".number");//refer to all number buttons and "."
+        this.operatorSelector=document.querySelectorAll(".operator");//refer to all operator buttons
+        this.clearButton=document.querySelector(".clear")
+
+        //set event listeners to take user inputs from button clicks
+        this.numberSelector.forEach(item=>item.addEventListener("click", this.saveNum));//save number to value on click
+        this.operatorSelector.forEach(item=>item.addEventListener("click", this.saveOperator));//save operator on click
+        this.clearButton.addEventListener("click", this.clearAll);
     }
-    //once operator is chosen, value has to be cleared so it's ready to store next number
-    //operator does not have to be cleared since it is just overwritten
-    value=""
-}
 
-//--------------------------------------------------------Evaluate Expression-------------------------------------------------------------------------//
-function evaluate(array) {
-    //map valuesArray to new array that changes values into number types
-    let expression = array.map(x=>isNaN(+x) ? x : +x);
-    //loop through array to find mult or div to complete first left to right (PEMDAS)
-    for(let i=0;i<expression.length; i++) {
-        //store two array elements on either side of current element
-        let a=expression[i-1];
-        let b=expression[i+1]
-        //if find multiplication, get product of a and b
-        if (expression[i]=="*"){
-            let x=a*b;
-            //in the array, replace elements corresponding to a, *, b with x
-            expression.splice(i-1, 3, x);
-            //i and i+1 are removed elements, and i-1 is now replaced by x
-            //therefore, we need to reduce i by 1 to accurately reflect where we are in array
-            i--;
+
+    //------------------------------------------------Storing Expressions from Event Listeners-----------------------------------------------//
+    //save arrow functions as variables, so when called with this.saveNum in event listener, saveNum can be called to act on event, not on this class object
+    //saveNum will collect entries from calclator and store in variable value as string
+    saveNum = (event) => {
+        //make sure there is no more than 1 decimal in a number
+        //reference button's value with event.target.value (since this is in a class, can't use this.value, as this refers to class object, and event object doesn't have a value property (it has target which has value))
+        if (event.target.value=="." && this.value.includes(".")) {
+            return this.value;
         }
-        //same program as above, for division:
-        else if (expression[i]=="/") {
-            let x=a/b;
-            expression.splice(i-1,3,x);
-            i--;       
+        //concatenate additional entries onto value (value will not be overwritten, so has to be cleared later after operator selected)
+        else {
+            this.value+=event.target.value;
+        }
+        //display value on screen (will display on each click of numbers as saveNum is run)
+        this.screen.innerText=this.value;
+        this.adjustFontSize();
+    }
+
+    //saveOperator will add previously stored value to array, store chosen operator, and evaluate when = is chosen
+    saveOperator = (event) => {
+        //save button's value to operator (will overwrite any previous operator)
+        this.operator=event.target.value;
+
+        //if "=" used, end expression and evaluate:
+        if (event.target.value=="=") {
+            //when = selected, add last stored value to valuesArray, if not a number, then store 0
+            if(this.value=="" || isNaN(+this.value)) {
+                console.log("no number entered, using 0");
+                this.value=0;
+            }
+            this.valuesArray.push(this.value);
+            //display entire array (ie the expression) on screen as a string
+            this.screen.innerText=this.valuesArray.join(' ');
+            //evaluate the expression: create evaluator with this.valuesArray, then use evaluator method
+            let evaluator = new Evaluator(this.valuesArray);
+            this.result = evaluator.evaluate();
+            this.showAnswer(this.result)
+            //clear the values array once expression is evaluated (will not allow additional calcuations on expression)
+            this.valuesArray=[];
+        }
+
+        //Allow making negative number if using "-" before entering a number. 
+            //Therefore only allow if there is no number stored in value already (doesn't matter if there is an operator stored or not)
+        else if (event.target.value=="-" && this.value=="") {
+            this.value=this.operator;
+            //escape this function so that value is not set to "", and "-" is assigned to value
+            return;
+        }
+
+        //If math operator chosen (except negation sign at beginning of number), store to expression:
+        else {
+            //once operator is chosen, push existing value to valuesArray
+            if (this.value!="" && this.value!="-") {
+                this.valuesArray.push(this.value);
+                this.valuesArray.push(this.operator);
+                this.screen.innerText=this.operator;
+            }
+            //if there is no value entered yet in expression, prompt user to enter number
+            else if(this.valuesArray.length==0) {
+                this.screen.innerText="Enter a number";
+            }
+            //if value is either 1)empty because already used an operatore and it cleared,
+            //or 2)or it is simply "-" without number, clicking operatore will mean that user wants to change operator
+            //therefore remove last element of valuesArray and push new operator
+            else if ((this.value=="" || this.value=="-") && this.valuesArray.length!=0){
+                console.log("changing operator");
+                this.valuesArray.splice(-1, 1, this.operator);
+                this.screen.innerText=this.operator;
+            }
+        }
+        //once operator is chosen, clear value so it's ready to store next number (operator does not have to be cleared since it is just overwritten)
+        this.value=""
+    }
+
+    clearAll = (event) => {
+        this.screen.innerText="";
+        this.value="";
+        this.valuesArray=[];
+        this.result=null;
+        this.screen.style.fontSize="3rem";
+    }
+
+
+    //-----------------------------------------------------Display functions-----------------------------------------------------------------//
+    showAnswer(answer) {
+        if (answer == "Infinity") {
+            answer = "Undefined"
+        }
+        this.screen.innerText+=` = ${answer}`;
+        this.adjustFontSize();
+    }
+
+    //determine if screen is large enough to fit characters on screen by comparing scrollWH to clientWH:
+    isOverFlow(){
+        //returns true if screen needs to be larger to fit text
+        return this.screen.scrollHeight>this.screen.clientHeight || this.screen.scrollWidth>this.screen.clientWidth;
+    }
+
+    adjustFontSize(){
+        let fontSize=parseInt(this.screen.style.fontSize);
+        for(let i=fontSize; i>0.6; i--){
+            let overflow=this.isOverFlow();
+            if (overflow) {
+                fontSize-=0.4;
+                this.screen.style.fontSize=fontSize+"rem"
+            }
         }
     }
-    //once mult and div are done, do addition and subtraction left to right by looping to resulting array
-    for(let i=0; i<expression.length; i++) {
-        let a=expression[i-1];
-        let b=expression[i+1];
-        if (expression[i]=="+") {
-            let x=a+b;
-            expression.splice(i-1,3,x);
-            i--;
-        }
-        else if (expression[i]=="-") {
-            let x=a-b;
-            expression.splice(i-1,3,x);
-            i--;
-        }
-        
-    }
-    result=expression[0];
-    showAnswer(result);
+
 }
 
-//-----------------------------------------------------Clear Function--------------------------------------------------------//
-let clearButton=document.querySelector(".clear")
-clearButton.addEventListener("click", clearAll);
 
-function clearAll() {
-    screen.innerText="";
-    value="";
-    valuesArray=[];
-    result=null;
-    screen.style.fontSize="3rem";
-}
+//Evaluate expressions
+class Evaluator {
+    //map string from expression array to numbers and operator methods to prepare for evaluation
+    constructor(expressionArray){
+        //turns all numeric strings to numbers, leaves operators as is
+        this.expression = expressionArray.map(x => isNaN(+x) ? x: +x);
 
-//-----------------------------------------------------Display---------------------------------------------------------------//
-function showAnswer(answer) {
-    document.querySelector(".screen").innerText+=` = ${answer}`;
-    adjustFontSize(screen);
-}
-
-//determine if screen is large enough to fit characters on screen by comparing scrollWH to clientWH:
-function isOverFlow(screen){
-    //returns true if screen needs to be larger to fit text
-    return screen.scrollHeight>screen.clientHeight || screen.scrollWidth>screen.clientWidth;
-}
-
-function adjustFontSize(screen){
-    let fontSize=parseInt(screen.style.fontSize);
-    for(let i=fontSize; i>0.6; i--){
-        let overflow=isOverFlow(screen);
-        if (overflow) {
-            fontSize-=0.4;
-            screen.style.fontSize=fontSize+"rem"
+        //maps operators to math function
+        this.methods = {
+        "+": (a, b) => a+b,
+        "-": (a, b) => a-b,
+        "*": (a, b) => a*b,
+        "/": (a, b) => a/b
         }
     }
+
+    //Evaluate the expression around any operator from left to right in expression
+    processOperators(operators) {
+        for(let i=0; i<this.expression.length; i++) {
+            let expression_item = this.expression[i]
+            if (operators.includes(expression_item)) {
+                let a=this.expression[i-1];
+                let b=this.expression[i+1];
+                let result = this.methods[expression_item](a,b)
+                //i and i+1 are removed elements, and i-1 is now replaced by result
+                this.expression.splice(i-1, 3, result);
+                //therefore, we need to reduce i by 1 to accurately reflect where we are in array
+                i--;
+            }
+        }
+    }
+
+    //Evaluate with order of operations
+    evaluate() {
+        //first process * and / in loop (processOperators runs left to right)
+        this.processOperators(["*", "/"]);
+        //then process + and - in loop
+        this.processOperators(["+", "-"]);
+        //return expression which should only have 1 element after going through above loops
+        return this.expression[0]
+    }
 }
+
+
+//Instantiate Calculator
+let calculator = new Calculator();
